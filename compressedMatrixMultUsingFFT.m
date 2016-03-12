9cebeeec2d053413c02a98b3744953699319b2b8
xingyu <xic12006@web10.uits.uconn.edu>
2016-03-12 16:54:33 -0500

    compressedUsingFFT

---
+++ compressedMatrixMultUsingFFT.m
@@ -0,0 +1,54 @@
+N = 1024;
+b = log2(N);
+d = 20;
+A = randn(N,N); B = randn(N,N);
+
+% --------------------- compressed product --------------
+s1_t = []; s2_t = []; h1_t = []; h2_t = []; p_t = [];
+for t = 1:d
+    s1 = sign(randn(1,N));
+    s2 = sign(randn(1,N));
+    h1 = ceil(rand(1,N).*b);
+    h2 = ceil(rand(1,N).*b);
+    p = zeros(1,b);
+    
+    for k = 1:N
+        pa = zeros(1,b); pb = zeros(1,b);
+        for i = 1:N
+            pa(h1(i)) = pa(h1(i))+s1(i)*A(i,k);
+        end
+        for j = 1:N
+            pb(h2(j)) = pb(h2(j))+s2(j)*B(j,k);
+        end
+        pa = fft(pa); pb = fft(pb);
+        for z = 1:b
+            p(z) = p(z)+pa(z)*pb(z);
+        end
+    end
+    
+    s1_t = [s1_t; s1];
+    s2_t = [s2_t; s2];
+    h1_t = [h1_t; h1];
+    h2_t = [h2_t; h2];
+    p_t = [p_t; p];
+end
+
+for t = 1:d
+    p_t(t,:) = ifft(p_t(t,:));
+end
+
+% ------------------- decompress (i,j) ------------------
+C_approx = zeros(N,N);
+for i = 1:N
+    for j = 1:N
+        for t = 1:d
+            X(t) = s1_t(t,i)*s2_t(t,j)*p_t(t,i)*mod(h1_t(t,i)+h2_t(t,j), b);
+        end
+        C_approx(i,j) = mean(X);
+    end
+end
+
+% ------------------- compare --------------------------
+C = A*B
+error = C - C_approx;
+fnorm = sum(error);
