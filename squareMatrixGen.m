c43b6502fdd197d02fe3af257f5698b8f27a84f5
xingyu <xingyu.develop.cai@gmail.com>
2016-07-04 15:59:12 -0400

    add square matrix generator

---
+++ squareMatrixGen.m
@@ -0,0 +1,44 @@
+% generate square matrix
+
+% input:
+% dim: dimension of matrix generated, will be changed to closest power of 2 values
+% type: dense, sparse, etc
+% dist: distribution of the matrix generated, 'normal', 'uniform', etc
+% if type == sparse, parameterList(1) will be density value 0<density<1
+
+% output: generated squared matrix
+
+function M = squareMatrixGen(dim, type, dist, parameterList);
+
+n = log2(dim);
+n = round(n);
+n = 2^n;
+
+if strcmp(type, 'dense')
+
+  if strcmp(dist, 'normal')
+    M = randn(n);
+
+  elseif strcmp(dist, 'uniform')
+    M = rand(n);
+
+  end
+
+
+elseif strcmp(type, 'sparse')
+  density = parameterList(1);
+
+  if strcmp(dist, 'normal')
+    M = sprandn(n, density);
+
+  elseif strcmp(dist, 'uniform')
+    M = sprand(n, density);
+
+  end
+
+
+end
+
+return;
+
+
