b1d63416ba7c27e3313e00c1f2243eb94a4c60cb
xingyu <xingyu.develop.cai@gmail.com>
2016-07-07 13:36:53 -0400

    add test run.m

--- run.m
+++ run.m
@@ -1,35 +1,40 @@
-run_time = 50;
-er = zeros(run_time, 8);
+% run_time = 50;
+% er = zeros(run_time, 8);
+% 
+% for i = 1:run_time
+%   basicMatrixMult;
+%   er(i,1) = error_norm/AB_norm;
+% 
+%   elementMatrixMult;
+%   er(i,2) = error_norm/AB_norm;
+% 
+%   randomProjMult;
+%   er(i,3) = error_norm/AB_norm;
+% 
+%   nystrom;
+%   er(i,4) = error_norm/AB_norm;
+%   
+%   compressedSensing;
+%   er(i,5) = error_norm/AB_norm;
+%   
+%   equationSolve;
+%   er(i,6) = error_norm/AB_norm;
+% 
+%   frequencyCounting;
+%   er(i,7) = error_norm/AB_norm;
+%   
+%   compressedFFT;
+%   er(i,8) = error_norm/AB_norm;
+% end
+% 
+% for j = 1:8
+%   er(i+1,j) = sum(er(1:i,j));
+% end
 
-for i = 1:run_time
-  basicMatrixMult;
-  er(i,1) = error_norm/AB_norm;
 
-  elementMatrixMult;
-  er(i,2) = error_norm/AB_norm;
+% ############# we can use ||C - C_approx || as a measurement of elementwise error #############################
 
-  randomProjMult;
-  er(i,3) = error_norm/AB_norm;
-
-  nystrom;
-  er(i,4) = error_norm/AB_norm;
-  
-  compressedSensing;
-  er(i,5) = error_norm/AB_norm;
-  
-  equationSolve;
-  er(i,6) = error_norm/AB_norm;
-
-  frequencyCounting;
-  er(i,7) = error_norm/AB_norm;
-  
-  compressedFFT;
-  er(i,8) = error_norm/AB_norm;
-end
-
-for j = 1:8
-  er(i+1,j) = sum(er(1:i,j));
-end
-
-
-
+% random matrix test 
+dim = 2^10;
+A = squareMatrixGen(dim, 'dense', 'normal');
+B = squareMatrixGen(dim, 'dense', 'normal');
