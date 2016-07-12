554291fbb92053f0d96d09bab5d37e3a47ff9921
xingyu <xingyu.develop.cai@gmail.com>
2016-07-12 13:24:07 -0400

    fix elementMatrixMult.m

--- elementMatrixMult.m
+++ elementMatrixMult.m
@@ -1,4 +1,3 @@
-% in Drines paper
 % A m*n, B n*p
 % sample c columns of A and corresponding rows of B
 % sampleType corresponds to sampleElement function input argument, including 'l1' and 'l2'
@@ -7,10 +6,8 @@
 function C_approx = elementMatrixMult(A, B, sampleType, parameterList);
 
 % ------------- multiplication routing -------------
-% the parameter l is to be tuned, here as default 1, but need to check paper
-l = parameterList(1);
-S = sampleElement(A, 'l2', l);
-R = sampleElement(B, 'l2', l);
+S = sampleElement(A, sampleType, parameterList);
+R = sampleElement(B, sampleType, parameterList);
 
 % sparsify representation significantly reduce computing time
 S = sparse(S);
