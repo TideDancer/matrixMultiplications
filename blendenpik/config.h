5319c7151270c50a89812baeb9076d14a3f9080f
xingyu <xingyu.develop.cai@gmail.com>
2016-07-23 12:09:40 -0400

    put blendenpik in the folder

---
+++ blendenpik/config.h
@@ -0,0 +1,6 @@
+#define FFTW_QUANT 1000
+#define FFTW_TIMES 150
+#define FFTW_WISDOM_FILE "/Users/xic12006/projects/blendenpik_v1.3/blendenpik/dat/wisdom_file.TD-MacMini.0.dat"
+#define FFTW_WISDOM_FLAG FFTW_ESTIMATE
+#define BLAS_UNDERSCORE
+#define USE_FFTW
