0200e3066aaec0b9eb18559096f29848f00dfcb2
TideDancer <xingyu.develop.cai@gmail.com>
2016-08-24 13:33:53 -0400

    test

--- run_all.m
+++ run_all.m
@@ -1,87 +1,86 @@
 settings;
-disp('--------------direct mult --------------');
 
 % ------------------------ column l2 sample -----------------------------------
+disp('-------------column 2 norm sampling-----------');
 tic;
 C_approx = basicMatrixMult(A, B, 'column2norm', [delta, epsilon, beta]);
 toc; 
 disp(strcat('errorRatio = ', num2str(norm(C-C_approx, 'fro')/AB_norm)))
-disp('-------------column 2 norm sampling-----------');
 
 
 % ------------------------ elementwise l2 ------------------------------------
+disp('-------------elementwise-------------');
 l = elementTest(A,B,epsilon); % default delta is 1/dim
 tic;
 C_approx = elementMatrixMult(A, B, 'l2', l); % l-2 based element-wise sampling
 toc;
 disp(strcat('errorRatio = ', num2str(norm(C-C_approx, 'fro')/AB_norm)))
-disp('-------------elementwise-------------');
 
 
 % ------------------------ fjlt ----------------------------------------
+disp('--------------fjlt -----------');
 tic;
 C_approx = randomProjMult(A, B, 'FJLT', [delta, epsilon, const]);
 toc;
 disp(strcat('errorRatio = ', num2str(norm(C-C_approx, 'fro')/AB_norm)))
-disp('--------------fjlt -----------');
 
 
 % ------------------------ tug-of-war ----------------------------------
+disp('--------------tug-of-war-------------');
 tic;
 C_approx = randomProjMult(A, B, 'tug-of-war', [delta, epsilon, const]);
 toc;
 disp(strcat('errorRatio = ', num2str(norm(C-C_approx, 'fro')/AB_norm)))
-disp('--------------tug-of-war-------------');
 
 
 % -------------------------- clarkson2009numerical -----------------------------
+disp('------------clarkson2009numerical---------------');
 tic;
 C_approx = randomProjMult(A, B, 'clarkson2009numerical', [delta, epsilon, const]); 
 toc;
 disp(strcat('errorRatio = ', num2str(norm(C-C_approx, 'fro')/AB_norm)))
-disp('------------clarkson2009numerical---------------');
 
 
 % ------------------------- kyrillidis2014approximate ---------------------------
+disp('--------------kyrillidis2014approximate--------------');
 tic;
 C_approx = randomProjMult(A, B, 'kyrillidis2014approximate', [delta, epsilon, const]);
 toc;
 disp(strcat('errorRatio = ', num2str(norm(C-C_approx, 'fro')/AB_norm)))
-disp('--------------kyrillidis2014approximate--------------');
 
 
 % --------------------------- compressedFFT ---------------------------
+disp('--------------compressedFFT---------------');
 tic;
 C_approx = compressedFFT(A, B, nnzAB);
 toc;
 disp(strcat('errorRatio = ', num2str(norm(C-C_approx, 'fro')/AB_norm)))
-disp('--------------compressedFFT---------------');
 
 
 % --------------------------- equationSolve ---------------------------
+disp('-------------equationSolve---------------');
 tic;
 C_approx = equationSolve(A, B);
 toc;
 disp(strcat('errorRatio = ', num2str(norm(C-C_approx, 'fro')/AB_norm)))
-disp('-------------equationSolve---------------');
 
 
 % -------------------------- nystrom --------------------------------------------
+disp('--------------nystrom---------------');
 tic;
 C_approx = nystrom(A, B, round(log10(dim)));
 toc;
 disp(strcat('errorRatio = ', num2str(norm(C-C_approx, 'fro')/AB_norm)))
-disp('--------------nystrom---------------');
 
 
 % ------------------------- cs based ---------------------------
+disp('--------------compressed sensing---------------');
 b = log(nnzAB)/log(dim) < 0.29462; % corollary 5 in the paper of cs-based mult
 [prob, gamma, c] = cGammaTest(C);  % c-gamma test to compute gamma for a specific C
 tic;
 C_approx = compressedSensing(A, B, [1, min(gamma)]);
 toc;
 disp(strcat('errorRatio = ', num2str(norm(C-C_approx, 'fro')/AB_norm)))
-disp('--------------compressed sensing---------------');
 
 
 
--- settings.m
+++ settings.m
@@ -1,5 +1,5 @@
 % define dimension when run
-dim = 2^15;
+dim = 2^15
 
 delta = 1/dim;  % failure probability
 epsilon = 1e-1; % error norm <= epsilon ||A|| ||B||
@@ -8,13 +8,30 @@ const = 1;
 
 density = 'dense';
 distribution = 'uniform';
-matrix = 'squareMatrixGen';
+matrix = 'high condition';
+cond_num = 10^5;
 
-A = squareMatrixGen(dim, density, distribution);
-B = squareMatrixGen(dim, density, distribution);
+% ------- geometrically distributed singular values ------
+% A = gallery('randsvd', dim ,cond_num, 3);
+% B = gallery('randsvd', dim ,cond_num, 3);
+% disp('gallery(randsvd, dim ,cond_num, 3)');
+
+% ------- square matrix ------
+%A = squareMatrixGen(dim, density, distribution);
+%B = squareMatrixGen(dim, density, distribution);
+
+% ------- crazy matrix --------
+A = gallery('sampling', dim);
+B = gallery('chebspec',dim,1);
+disp('gallery(sampling,dim),gallery(chebspec,dim,1)');
+
+% -------------------------- regular -------------------------------------
+disp('-----------------regular mult ----------------------');
 tic;
 C = A*B;
 toc;
+
+
 nnzAB = nnz(C);
 A_norm = norm(A, 'fro');
 B_norm = norm(B, 'fro');
