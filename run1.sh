a6999954ec3f30471b0ffb9ad7d613a219e48e97
Xingyu Cai {xingyu.cai@uconn.edu} <xic12006@cn03.(none)>
2016-10-19 20:02:59 -0400

    start running

--- run1.sh
+++ run1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#SBATCH -n 1
+#SBATCH -n 4
 #SBATCH -o outfile.txt
 #SBATCH -e errfile.txt
+#SBATCH --partition=HaswellPriority
+#SBATCH --account=rajasek
 
 srun -l --multi-prog run1.conf
